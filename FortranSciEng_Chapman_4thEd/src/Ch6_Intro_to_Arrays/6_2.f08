f5b477db132cffd3885930b2e4399431e2c56bda
S <ssoehnlen@protonmail.com>
2026-05-01 20:37:48 +0200

    edits to ch6 src

--- FortranSciEng_Chapman_4thEd/src/Ch6_Intro_to_Arrays/6_2.f08
+++ FortranSciEng_Chapman_4thEd/src/Ch6_Intro_to_Arrays/6_2.f08
@@ -1,3 +1,8 @@
+! Demonstrates use of assignment statements to 
+! initialize values in an array. 
+! %%%%%%%%%%%%%%%%%%%%%
+! Calculates the squares of integers from 1 to 10
+
 PROGRAM squares 
     IMPLICIT NONE 
 
@@ -7,11 +12,11 @@ PROGRAM squares
     ! Initialize number and calculate square 
     DO i = 1, 10 
         number(i) = i                  ! Initialize number 
-        square(i) = number(i)**2         ! Calculate square 
+        square(i) = number(i)**2          ! Calculate square 
     END DO 
 
     ! Write out each number and its square 
-    DO i = 1, 10 
+    DO i = 1, 10
         WRITE (*, 100) number(i), square(i)
         100 FORMAT ('Number = ',I6,' Square = ',I6)
     END DO 
---
+++ FortranSciEng_Chapman_4thEd/src/Ch6_Intro_to_Arrays/6_3.f90
@@ -0,0 +1,24 @@
+! Demonstrates using a type declaration statement to assign 
+! initialize the values in an array.
+! %%%%%%%%%%%%%%%%%%%%%
+! Calculates the square roots of the integers 1 to 10. 
+
+PROGRAM square_roots 
+    IMPLICIT NONE 
+
+    INTEGER :: i 
+    REAL, DIMENSION(10) :: value = [ (i, i=1,10) ] 
+    REAL, DIMENSION(10) :: square_root 
+
+    ! Calculate the square roots of the numbers. 
+    DO i = 1, 10 
+        square_root(i) = SQRT(value(i))
+    END DO 
+
+    ! Write out each number and its square root. 
+    DO i = 1, 10
+        WRITE (*, 100) value(i), square_root(i) 
+        100 FORMAT ('Value = ' , F5.1, ' Square Root = ', F10.4)
+    END DO 
+
+    END PROGRAM square_roots
