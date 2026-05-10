70b75aac14d77077cd571db55d12c502cf7cf787
S <ssoehnlen@protonmail.com>
2026-05-10 12:33:33 +0200

    added notes to bounds check 6_6.f90

--- FortranSciEng_Chapman_4thEd/src/Ch6_Intro_to_Arrays/6_6.f90
+++ FortranSciEng_Chapman_4thEd/src/Ch6_Intro_to_Arrays/6_6.f90
@@ -1,6 +1,37 @@
 ! Example 6-6 
 ! Illustrate the effect of out-of-bounds array references with 
-! and without bounds checking turned on. 
+! and without bounds checking turned on.
+! (IFX produces identical error messages) 
+
+! with bound checking on (win OS environment flags):
+! ifort -check file_name.f90 -o filename 
+
+! Compiling without the -check flag will produce a binary that 
+! will yield some erroneous values for the 6th array element 
+!       (e.g. junk memory, next element in adjacent array etc., 
+!       such as an array value of '0' for element a(6)
+!       in this example for a(1) -a(5) ),
+! whereas with bounds checking on, a descriptive error message with
+! the results up to array element 6 are produced: 
+
+! #######################################################################################################################
+! Output with bounds checking on (ifort compiler)
+! a(1) =   1.00
+! a(2) =   2.00
+! a(3) =   3.00
+! a(4) =   4.00
+! a(5) =   5.00
+! forrtl: severe (408): fort: (10): Subscript #1 of the array A has value 6 which is greater than the upper bound of 5
+
+! Image              PC                Routine            Line        Source
+! boundCheck.exe     00007FF6A901116E  Unknown               Unknown  Unknown
+! boundCheck.exe     00007FF6A904F0FB  Unknown               Unknown  Unknown
+! boundCheck.exe     00007FF6A904F524  Unknown               Unknown  Unknown
+! KERNEL32.DLL       00007FFF8CA4E8D7  Unknown               Unknown  Unknown
+! ntdll.dll          00007FFF8D94C53C  Unknown               Unknown  Unknown
+!########################################################################################################################
+
+
 PROGRAM bounds 
     IMPLICIT NONE 
 ! Declare and initialize the variables used
