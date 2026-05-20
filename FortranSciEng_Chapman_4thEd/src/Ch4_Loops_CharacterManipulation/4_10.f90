6397521c91fb18314c940d0c9e1a8d0a7dd601a6
S <ssoehnlen@protonmail.com>
2026-05-20 18:53:36 +0200

    clean up

--- FortranSciEng_Chapman_4thEd/src/Ch4_Loops_CharacterManipulation/4_10.f90
+++ FortranSciEng_Chapman_4thEd/src/Ch4_Loops_CharacterManipulation/4_10.f90
@@ -1,48 +1,47 @@
 ! Figure 4-10
-PROGRAM compare 
+PROGRAM compare
 
-! Purpose: 
+! Purpose:
 !   To compare two strings to see if they are equivalen,
-!   ignoring case. 
+!   ignoring case.
 
-    IMPLICIT NONE 
+    IMPLICIT NONE
 
 !   Data dictionary: declare variable types, definitions, & units
-    
-    INTEGER :: i 
+
+    INTEGER :: i
     CHARACTER(len=20) :: str1   ! First string to compare
-    CHARACTER(len=20) :: str1a  ! Copy of first string to compare 
-    CHARACTER(len=20) :: str2   ! Second string to compare 
-    CHARACTER(len=20) :: str2a  ! Copy of second string to compare 
+    CHARACTER(len=20) :: str1a  ! Copy of first string to compare
+    CHARACTER(len=20) :: str2   ! Second string to compare
+    CHARACTER(len=20) :: str2a  ! Copy of second string to compare
 
-!    Prompt for the strings 
+!    Prompt for the strings
     WRITE (*,*) 'Enter first string to compare:'
-    READ (*,*) str1 
-    WRITE (*,*) 'Enter second string to compare:' 
-    READ (*,*) str2 
+    READ  (*,*) str1
+    WRITE (*,*) 'Enter second string to compare:'
+    READ  (*,*) str2
 
-!   Make copies so that the original strings are not modified 
-    str1a = str1 
-    str2a = str2 
+!   Make copies so that the original strings are not modified
+    str1a = str1
+    str2a = str2
 
-!   Now shift lowercase letters to uppercase. 
+!   Now shift lowercase letters to uppercase.
     DO i = 1, LEN(str1a)
-        IF (str1a(i:i) >= 'a' .AND. str1a(i:i) <= 'z' ) THEN 
+        IF (str1a(i:i) >= 'a' .AND. str1a(i:i) <= 'z' ) THEN
             str1a(i:i) = ACHAR ( IACHAR ( str1a(i:i) ) -32 )
-        END IF 
-    END DO 
+        END IF
+    END DO
     DO i = 1, LEN(str2a)
-        IF ( str2a(i:i) >= 'a' .AND. str2a(i:i) <= 'z' ) THEN 
+        IF ( str2a(i:i) >= 'a' .AND. str2a(i:i) <= 'z' ) THEN
             str2a(i:i) = ACHAR ( ICHAR (str2a(i:i) ) - 32 )
-        END IF 
-    END DO 
+        END IF
+    END DO
 
 !   Compare strings and write result
-    IF ( str1a == str2a ) THEN 
+    IF ( str1a == str2a ) THEN
         WRITE (*,*) " ' ", str1, " ' = ' ", str2, " ' ignoring case."
-    ELSE 
+    ELSE
         WRITE (*,*) " ' ", str1, " ' /= ' ", str2, " ' ignoring case."
     END IF
 
-END PROGRAM compare 
-    
+END PROGRAM compare
