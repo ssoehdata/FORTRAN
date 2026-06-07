64b78ae1cdc695a7673cc7093455182e31b5f696
S <ssoehnlen@protonmail.com>
2026-06-07 17:56:44 +0200

    rename vars

--- Fortran90SciEng/Ch6/approxInt.f90
+++ Fortran90SciEng/Ch6/approxInt.f90
@@ -19,37 +19,37 @@
 
 PROGRAM Approximate_Integration
     IMPLICIT NONE
-    REAL :: A, B, DeltaX, X, Y, Sum
-    Integer :: N, I
+    REAL :: A_lower_bound, B_upper_bound, DeltaX, X, Y, Sum
+    Integer :: N_num_subintervals, I
     PRINT *, "Enter the interval endpoints and the number of subintervals: "
-    READ *, A, B, N
+    READ *, A_lower_bound, B_upper_bound, N_num_subintervals
 
     ! Calculate subinterval length
     ! and initialize the approximating Sum and x
 
-    DeltaX = (b - A) / REAL(N)
-    X = A
+    DeltaX = (b_upper_bound - A_lower_bound) / REAL(N_num_subintervals)
+    X = A_lower_bound
     Sum = 0.0
 
-    DO I = 1, N -1
+    DO I = 1, N_num_subintervals - 1
         X = X + DeltaX
         Y = F(X)
         Sum = Sum + Y
     END DO
 
-    Sum = DeltaX * (( F (A) + F(B)) / 2.0 + Sum)
+    Sum = DeltaX * (( F (A_lower_bound) + F(B_upper_bound)) / 2.0 + Sum)
     PRINT *
     PRINT  '(1X, "Approximate value using ", I4, &
-        " subintervals is", F10.5)', N, Sum
+        " subintervals is", F10.5)', N_num_subintervals, Sum
     PRINT *
     PRINT  '(1X, "Using Scientific Notation ", I4, &
-        " subintervals is", ES15.5)', N, Sum
+        " subintervals is", ES15.5)', N_num_subintervals, Sum
     PRINT *
     PRINT  '(1X, "Using Engineering Notation ", I4, &
-        " subintervals is", EN15.5)', N, Sum
-    PRINT *   
+        " subintervals is", EN15.5)', N_num_subintervals, Sum
+    PRINT *
     PRINT  '(1X, "Using Exponential Notation ", I4, &
-        " subintervals is", E15.5)', N, Sum
+        " subintervals is", E15.5)', N_num_subintervals, Sum
     PRINT *
 
     CONTAINS
