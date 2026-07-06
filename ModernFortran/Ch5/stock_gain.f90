314244c3adad2b77b30542596ef6e90433996ce8
s <ssoehnlen@protonmail.com>
2026-07-06 09:47:53 +0200

    adding part of ch5 src

---
+++ ModernFortran/Ch5/stock_gain.f90
@@ -0,0 +1,42 @@
+PROGRAM stock_gain 
+
+        use mod_arrays, only: reverse 
+        use mod_io, only: read_stock 
+
+        IMPLICIT NONE 
+
+        character(len=4), allocatable :: symbols(:) 
+        character(len=:), allocatable :: time(:)
+        real, allocatable :: open(:), high(:), low(:), &
+                close(:), adjclose(:), volume(:)
+
+        integer :: n 
+        real    :: gain 
+
+        symbols = ['AAPL', 'AMZN', 'CRAY', 'CSCO', 'HPQ ',&
+             'IBM ', 'INTC', 'MSFT', 'NVDA', 'ORCL']
+
+       do n = 1, size(symbols)
+
+        call read_stock(                               &
+                'data/' // trim(symbols(n)) // '.csv', &
+                        time, open, high, low, close, adjclose, volume)
+
+       adjclose = reverse(adjclose)
+       gain = (adjclose(size(adjclose)) - adjclose(1))
+
+       if (n == 1) then 
+               print *
+               print *, &
+                       time(size(time)) // 'through' // time(1)
+               print *, 'Symbol, Gain (USD), Relative gain (%)'
+               print *, '-------------------------------------'
+      end if 
+
+                print *, symbols(n), gain, nint(gain / adjclose(1) * 100)
+     
+    end do
+    print *
+
+
+END PROGRAM stock_gain
